a225f6c54d39ec0ad91f58b58d959868e70ac7ab
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-11 19:07:03 -0500

    wrong filename

--- update.php
+++ update.php
@@ -1,5 +1,5 @@
 <?php
-$tmp = fopen("testfile.txt", "r") or die("Unable to open file!");
-echo fread($tmp,filesize("testfile.txt"));
+$tmp = fopen("newfile.txt", "r") or die("Unable to open file!");
+echo fread($tmp,filesize("newfile.txt"));
 fclose($tmp);
 ?>
