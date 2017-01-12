a8ada59d24bc10eb4b0b9b477a252a14e12a2b35
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-11 21:23:50 -0500

    HTML style new lines

--- recieve.php
+++ recieve.php
@@ -1,12 +1,11 @@
 <?php
 header('Content-type: application/json');
 $data= $_POST['data'];
-$tmp = json_encode($data);
-$decoded = json_decode($tmp, true);
+$decoded = json_decode($data, true);
 echo $decoded;
 
 $tmpFile = fopen("newfile.txt", "a") or die("Unable to open file!");
-fwrite($tmpFile, "\r\n");
+fwrite($tmpFile, "<br>");
 fwrite($tmpFile, $decoded['Info']);
 fwrite($tmpFile, " recieved at ");
 fwrite($tmpFile, $decoded['Time']);
