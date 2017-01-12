e34e581a81304d266f922e8a2f0644e35b99c8e0
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-12 01:48:04 -0500

    Recieve.php Old Method 3

--- recieve.php
+++ recieve.php
@@ -1,8 +1,8 @@
 <?php
-//header('Content-type: application/json');
-//$dataIn= $_POST['data'];
-//$dateEncode = json_encode($dataIn); //only way to get data out of a recieved json file
-//$data = json_decode($dateEncode, true); //only way to convert to array
+header('Content-type: application/json');
+$dataIn= $_POST['data'];
+$dateEncode = json_encode($dataIn); //only way to get data out of a recieved json file
+$data = json_decode($dateEncode, true); //only way to convert to array
 //echo $data;
 
 $file="log.txt";
