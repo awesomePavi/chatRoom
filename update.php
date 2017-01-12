1b2d74a00132cbc0ae079a22ac9fbb6e9433844e
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-12 02:02:10 -0500

    Recieve.php Old Method 9

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-$tmp = fopen("data1.json", "r") or die("Unable to open file!");
+$tmp = fopen("CHAT.txt", "r") or die("Unable to open file!");
 echo fread($tmp,filesize("CHAT.txt"));
 fclose($tmp);
 ?>
