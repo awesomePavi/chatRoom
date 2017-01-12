195e2596186676db61d76028685243ff2cb6653d
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-12 01:19:59 -0500

    Recieve.php Debug 4

--- recieve.php
+++ recieve.php
@@ -3,6 +3,11 @@ header('Content-type: application/json');
 $data= $_POST['data'];
 echo $data;
 
+$tmpFile = fopen("log.txt", "w") or die("Unable to open file!");
+fwrite($tmpFil, $data);
+fclose($tmpFil);
+
+
 /*
 $decoded['userName'];
 $decoded['Message'];
@@ -17,7 +22,7 @@ fclose($jsonFile);
 
 $jsonFile = fopen("data1.json", "w") or die("Unable to open file!");
 $allPos[count($allPos)] = $decoded;
-fwrite($jsonFile, json_encode($allPos););
+fwrite($jsonFile, json_encode($allPos));
 fclose($jsonFile);
 */
 ?>
