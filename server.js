8bc926a386052f360bbd39ba304fa77038f6f564
Pavithran Pathmarajah <pavithranpath@gmail.com>
2017-01-18 17:02:47 -0500

    V1.1 total user logins, message history

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var http = require('http').Server(app);
 //websockets, on establised http listener
 var io = require('socket.io')(http);
 
+var users = [];
+var messagelogs = [];
+
 //response for get response for applet
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/chat.html');
@@ -12,10 +15,32 @@ app.get('/', function(req, res){
 
 //handle all  connections, and what happens
 io.on('connection', function(socket){
-	// when a connected machine sends a message
+
+  socket.on('fresh', function(name){
+    socket.emit('message','Server',"WelcomeBro");
+    socket.emit('messageHistory', {data:messagelogs});
+    
+     //send message to all connected machines
+    
+    io.emit('message', name, "Has Joined The Chat");
+     var tmp = {user:name, message: "Has Joined The Chat"};
+     messagelogs.push(tmp);
+    users.push(name);
+
+    tmp = {numUsers:users.length, numMsgs:messagelogs.length};
+     io.emit('roomInfo', {data:tmp});
+  });
+
+   // when a connected machine sends a message
   socket.on('message', function(name,msg){
-  	//send message to all connected machines
+  	 //send message to all connected machines
     io.emit('message', name,msg);
+    
+    var tmp = {user:name, message: msg};
+    messagelogs.push(tmp);
+
+    tmp = {numUsers:users.length, numMsgs:messagelogs.length};
+    io.emit('roomInfo', {data:tmp});
   });
 });
 
